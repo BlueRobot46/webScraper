414f01fc789e9fbd4fb6a4699ef8b42dc52a4cf5
Ben Tran <bambootran68@gmail.com>
2020-03-06 13:41:41 -0600

    Added full options to fully login to website

--- webScraper.py
+++ webScraper.py
@@ -3,7 +3,8 @@
 
 import sys
 from selenium import webdriver
-import time
+from selenium.webdriver.common.keys import Keys
+import time, pyperclip
 
 #User info question
 while True:
@@ -37,6 +38,8 @@ while len(accountValue) < userNum:
     print(numUser)
     print(account)
     print(accountValue)
+#Create a list, wrap the code for opening chrome in a while loop break out of the loop once the length of the code reaches a certain number.
+chromeIteration = []
 
 # Calls on webdriver module to open chrome.
 driver = webdriver.Chrome('C:\webDrivers/chromedriver.exe')
@@ -44,5 +47,21 @@ driver = webdriver.Chrome('C:\webDrivers/chromedriver.exe')
 driver.get("https://www.nike.com/launch")
 
 time.sleep(3);
-button = driver.find_element_by_css_selector('#root > div > div > div.main-layout > div > header > div.d-sm-h.d-lg-b > section > ul > li.member-nav-item.d-sm-ib.va-sm-m > button')
-clickButton = button.click();
+button1 = driver.find_element_by_css_selector('#root > div > div > div.main-layout > div > header > div.d-sm-h.d-lg-b > section > ul > li.member-nav-item.d-sm-ib.va-sm-m > button')
+clickButton = button1.click();
+
+#inputing Username
+pyperclip.copy(account[0]);
+button2 = driver.find_element_by_xpath('/html/body/div[2]/div/div/div[2]/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div[2]/form/div[2]/input');
+button2.click();
+button2.send_keys(Keys.CONTROL, 'v');
+
+#inputing Username value
+pyperclip.copy(accountValue[0])
+button3 = driver.find_element_by_xpath('/html/body/div[2]/div/div/div[2]/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div[2]/form/div[3]/input')
+button3.click();
+button3.send_keys(Keys.CONTROL, 'v')
+
+#Login
+loginButton = driver.find_element_by_xpath('/html/body/div[2]/div/div/div[2]/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div[2]/form/div[6]/input')
+loginButton.click();
